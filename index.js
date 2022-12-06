ea72a1f9306cf155bf8fd8e450571c8f83f6e5fd
Keithly009 <113906981+Keithly009@users.noreply.github.com>
2022-12-06 04:01:16 -0800

    finished the lib folders with Intern/Engineer/Manager

--- index.js
+++ index.js
@@ -6,3 +6,24 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 //TODO - write your inquirer app here to gather information about the team members, and generate the HTML file using fs
+
+inquirer.prompt([
+    {
+        type: 'list',
+        name: 'What is the position of this employee?',
+        choices: [
+            'Manager', 
+            'Intern', 
+            'Engineer'
+        ]
+    }
+]) .then(({ position }) => { 
+    switch(position) { 
+        case ' Manager': 
+
+        case ' Intern' : 
+
+        case ' Engineer': 
+        
+    }
+})
