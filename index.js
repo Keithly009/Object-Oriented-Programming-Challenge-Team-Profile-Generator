a8feb10c57a7014f7f2a5511c6f53705539a7cdb
Keithly009 <113906981+Keithly009@users.noreply.github.com>
2022-12-06 04:38:50 -0800

    Added code to js detailing the input from user for questions, problem with name / email however

--- index.js
+++ index.js
@@ -5,25 +5,88 @@ const Intern = require('./lib/Intern');
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+const employees = []
+
 //TODO - write your inquirer app here to gather information about the team members, and generate the HTML file using fs
 
-inquirer.prompt([
-    {
-        type: 'list',
-        name: 'What is the position of this employee?',
-        choices: [
-            'Manager', 
-            'Intern', 
-            'Engineer'
-        ]
-    }
-]) .then(({ position }) => { 
-    switch(position) { 
-        case ' Manager': 
+function newEmployee() { 
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'position',
+            message: 'What is the position of this employee?',
+            choices: [
+                'Manager', 
+                'Intern', 
+                'Engineer'
+            ]
+        },
+        {
+            type: 'input',
+            name: 'Name',
+            message: 'What is the name of this employee?',
+        },
+        {
+            type: 'input',
+            name: 'Email',
+            message: 'What is the email address this employee would use?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the id of this employee?',
+        }
+    ]) .then(({ position, email, id, name }) => { 
+        switch(position) { 
+            case 'Manager': 
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'officeNumber',
+                    message: 'Please provide the Office number.'
+                }
+            ]).then (({ officeNumber }) => {
+                employees.push(new Manager(
+                    name, 
+                    id,
+                    email,
+                    officeNumber
+                ))
+                
+                another()
+                
+            })
+    // If chose Manager, ask about office Number 
+            
+            break;
+            case ' Intern' : 
 
-        case ' Intern' : 
+    // If Chose Intern, ask about which School they are from
+            break;
+            case ' Engineer': 
 
-        case ' Engineer': 
+    // If chose Engineer, ask about their Github
+
+            break;
+            default: 
+            // There was nothing that applied to them
         
-    }
-})
+        }
+    })
+}
+
+function another() { 
+    return inquirer.prompt([
+        {
+            type: 'confirm', 
+            name: 'more',
+            message: 'Is there another employee you would like to add?'
+        }
+    ]).then(({ more }) => {
+        if(more) newEmployee()
+        console.log(employees)
+        // else renderHTMLFile()
+    })
+}
+
+newEmployee()
